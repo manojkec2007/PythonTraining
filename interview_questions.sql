ba92fdfe84917826048c34b72f7d57d5a66ba15e
Manoj KV <manojkvcts@gmail.com>
2017-04-30 22:29:26 -0700

    Upload Python Scripts

---
+++ interview_questions.sql
@@ -0,0 +1,59 @@
+http://www.techbeamers.com/python-interview-questions-experienced/
+
+https://www.tutorialspoint.com/python/python_interview_questions.htm
+
+http://www.techbeamers.com/python-interview-questions-programmers/
+
+https://github.com/zhiwehu/Python-programming-exercises/blob/master/100%2B%20Python%20challenging%20programming%20exercises.txt
+
+www.sanfoundry.com/1000-python-questions-answers/
+
+https://www.dezyre.com/article/100-data-science-in-python-interview-questions-and-answers-for-2017/188
+
+
+http://stackoverflow.com/questions/419163/what-does-if-name-main-do
+https://techmyway.wordpress.com/2011/06/17/python-interview-questions/
+https://blog.interviewmocha.com/python-interview-questions-for-experienced/
+http://www.ilian.io/python-interview-question-and-answers/
+http://www.testingbrain.com/interview/python-interview-questions.html/2
+http://www.interviewquestionspdfs.com/teradata-interview-questions-and-answers-pdf/
+
+
+--Questions:
+1.What are Mutable and Immutable Data structures in Python ?
+2.What are tuples ?
+3.What is difference between tuple and list ? Where will you use tuple and where will you use list ?
+4.What is Dynamic Typing ?
+5.Justify this statement : Everything is object in Python ?
+6.Python is Call by Value or Call by Reference ?
+7.How do you create a dictionary which can preserve the order of pairs?
+8.Can you use mutable Data Structure as key in Dictionaries ?
+9.What is the use of enumerate() in Python?
+10.What are *args, **kwargs ?
+11.How instance variables are different from class variables?
+12.Differentiate between “*.py” file and “*.pyc” file?
+13.Explain difference between Map vs Reduce Vs Filter ?
+14.What are Lamda Functions ?
+15.What is List comprehension ?
+16.What is __init__ functions ?
+17.What are Generators ?
+18.What are Iterators ?
+19.Can generator be used to create Iterators ? Give example
+20.Can iterators be used to create generator ?
+21.Differentiate between Range and Xrange ?
+22.What is Method Resolution Order ?
+23.Python supports Multi-Level Inheritance or Multiple Inheritance or Both ?
+24.Does Python supports multi-threading ?
+25.What is multi-threading? What is GIL(Global interpreter lock) issue ?
+26.What are decorator ? How to create custom decorator ?
+27.How memory is managed in Python ?
+28.How does Pythons garbage collection work?
+29.What are Regular Expressions ?
+30.Differentiate between append() and extend() methods ?
+31.What is Web Scraping? How do you achieve it in Python?
+32.Explain the use “with” statement in python?
+33.What are Middlewares ?
+34.What is Monkey patching ? Give example ?
+35.Whats the difference between py2.x and py3.x ?
+36.Give examples of Python Framework 
+
